108922dd9d197b2415bec742a6e04fd9c186724d
Omar Costilla <omarcostilla6@gmail.com>
2022-04-27 13:24:33 -0700

    ready to migrate code to fragment

---
+++ app/src/main/java/com/ecs198f/foodtrucks/FoodTruckListFragment.kt
@@ -0,0 +1,19 @@
+package com.ecs198f.foodtrucks
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FoodTruckListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_food_truck_list, container, false)
+    }
+
+}
